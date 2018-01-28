6bf2dd9738849408eb6407967c40c494df18e087
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-01-28 15:29:56 -0800

    normalize, tweak params

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.datasets import cifar100
 from keras.utils import to_categorical
+import keras
 
 (x_train, y_train), (x_test, y_test) = cifar100.load_data(label_mode='fine')
 
@@ -9,39 +10,28 @@ y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 print(y_train)
 
-"""
-[
-    [1],
-    [2],
-    [3]
-]
-
-[
-    [1, 0, 0],
-    [0, 1, 0],
-    [0, 0, 1]
-]
-"""
-
 def reshapeX(x):
     dim = ( x.shape[0], x.shape[1]*x.shape[2]*x.shape[3])
-    return x.reshape(dim)
+    return keras.utils.normalize(x.reshape(dim))
 
 x_train = reshapeX(x_train)
 x_test = reshapeX(x_test)
 model = Sequential()
 
 
-model.add(Dense(units=64, activation='relu', input_dim=x_train.shape[1]))
-model.add(Dense(units=64, activation='relu', input_dim=x_train.shape[1]))
-model.add(Dense(units=100, activation='softmax'))
+for i in range(20):
+    model.add(Dense(units=100, activation='relu', input_dim=x_train.shape[1], kernel_initializer='he_normal'))
+model.add(Dense(units=100, activation='softmax', kernel_initializer='he_normal'))
+
+#sgd = keras.optimizers.SGD(lr=2.0, momentum=0.0, decay=0.0, nesterov=False)
 
 model.compile(loss='categorical_crossentropy',
+              #optimizer=sgd,
               optimizer='sgd',
               metrics=['accuracy'])
 
 print(x_train.shape, y_train.shape)
-model.fit(x_train, y_train, epochs=5, batch_size=32)
+model.fit(x_train, y_train, epochs=20, batch_size=32)
 
 loss, acc = model.evaluate(x_test, y_test, batch_size=128)
 
